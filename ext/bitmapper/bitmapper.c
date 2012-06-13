c9d9c652e89fb818ffc0ea1fc32df9d26ae3f287
Goutham Vel <gouthamvel@gmail.com>
2012-06-13 13:04:04 +0530

    adds get_indexes && binary string load and dump functionality

--- ext/bitmapper/bitmapper.c
+++ ext/bitmapper/bitmapper.c
@@ -25,7 +25,38 @@ VALUE bm_alloc(VALUE self){
   return Data_Wrap_Struct(self, 0, bm_free, map);
 }
 
-VALUE bm_add_num(VALUE self, VALUE file_str){
+/*
+   Generates current index and assign it to @indexes variable
+ */
+VALUE bm_referesh_indexes(VALUE self){
+  Bitmapper* map;
+  ID id_push;
+  VALUE arr;
+  FILE *fp;
+  unsigned long long int i;
+  arr = rb_ary_new();
+  id_push = rb_intern("push");
+  Data_Get_Struct(self, Bitmapper, map);
+
+  rb_funcall(arr, rb_intern("clear"), 0);
+  for(i=0;i<map->bkt_count;i++)
+    if(map->bkts[i] != NULL)
+      rb_funcall(arr, id_push, 1, LL2NUM(i));
+
+  rb_iv_set(self, "@indexes", arr);
+  return self;
+}
+
+/*
+   Call to refresh indexs in @indexes
+   and return the value
+ */
+VALUE bm_get_indexes(VALUE self){
+  bm_referesh_indexes(self);
+  return rb_iv_get(self, "@indexes");
+}
+
+VALUE bm_add_from_file(VALUE self, VALUE file_str){
   Bitmapper* map;
   VALUE index_len;
   FILE *fp;
@@ -37,7 +68,19 @@ VALUE bm_add_num(VALUE self, VALUE file_str){
   return self;
 }
 
-VALUE bm_dump_num(VALUE self, VALUE file_str){
+VALUE bm_remove_from_file(VALUE self, VALUE file_str){
+  Bitmapper* map;
+  VALUE index_len;
+  FILE *fp;
+  index_len = rb_iv_get(self, "@index_len");
+  Data_Get_Struct(self, Bitmapper, map);
+  fp = fopen(RSTRING_PTR(StringValue(file_str)), "r");
+  remove_numbers_in_file(map, fp, NUM2LL(index_len));
+  fclose(fp);
+  return self;
+}
+
+VALUE bm_dump_to_file(VALUE self, VALUE file_str){
   Bitmapper* map;
   FILE *fp;
   Data_Get_Struct(self, Bitmapper, map);
@@ -47,17 +90,45 @@ VALUE bm_dump_num(VALUE self, VALUE file_str){
   return self;
 }
 
+VALUE bm_load_str_to_bkt(VALUE self, VALUE file_str, VALUE index){
+  Bitmapper* map;
+  FILE *fp;
+  Data_Get_Struct(self, Bitmapper, map);
+  fp = fopen(RSTRING_PTR(StringValue(file_str)), "r");
+  load_str_file_to_bucket(map, fp, NUM2LL(index));
+  fclose(fp);
+  return self;
+}
+
+VALUE bm_dump_bkt_str(VALUE self, VALUE file_str, VALUE index){
+  Bitmapper* map;
+  FILE *fp;
+  Data_Get_Struct(self, Bitmapper, map);
+  fp = fopen(RSTRING_PTR(StringValue(file_str)), "w");
+  dump_bucket_str_to_file(map, fp, NUM2LL(index));
+  fclose(fp);
+  return self;
+}
+
+/*
+   returns true if the provided number is present in bitmap or false
+ */
+VALUE bm_num_status(VALUE self, VALUE num){
+
+}
+
 void Init_bitmapper(){
   rb_cBitmapper = rb_define_class("Bitmapper", rb_cObject);
   rb_define_alloc_func(rb_cBitmapper, bm_alloc);
   rb_define_method(rb_cBitmapper, "initialize", bm_init, 1);
-  rb_define_method(rb_cBitmapper, "add_numbers", bm_add_num, 1);
-  rb_define_method(rb_cBitmapper, "dump_numbers", bm_dump_num, 1);
-  /* rb_define_method(rb_cBitmapper, "remove_numbers", bm_remove_num, 1); */
-  /* rb_define_method(rb_cBitmapper, "inject_string", bm_inj_str, 1); */
-  /* rb_define_method(rb_cBitmapper, "dump_string", bm_dump_str, 1); */
+  rb_define_method(rb_cBitmapper, "get_indexes", bm_get_indexes, 0);
+  rb_define_method(rb_cBitmapper, "add_from_file", bm_add_from_file, 1);
+  rb_define_method(rb_cBitmapper, "remove_from_file", bm_remove_from_file, 1);
+  rb_define_method(rb_cBitmapper, "dump_to_file", bm_dump_to_file, 1);
+  rb_define_method(rb_cBitmapper, "load_string_to_bucket", bm_load_str_to_bkt, 2);
+  rb_define_method(rb_cBitmapper, "dump_bucket_string", bm_dump_bkt_str, 2);
 
-  /* rb_define_method(rb_cBitmapper, "status?", bm_num_status, 1); */
+  rb_define_method(rb_cBitmapper, "status?", bm_num_status, 1);
   /* rb_define_method(rb_cBitmapper, "set", bm_set, 1); */
   /* rb_define_method(rb_cBitmapper, "clear", bm_clear, 1); */
 
