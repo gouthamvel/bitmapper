427d1ca2febd0333145253ee7cce73e5f645c9d8
Goutham Vel <gouthamvel@gmail.com>
2012-06-14 16:23:36 +0530

    change in test for name change

--- test/test_bitmapper.rb
+++ test/test_bitmapper.rb
@@ -62,37 +62,37 @@ class BitmapperTest < Test::Unit::TestCase
     p "#{id} -- #{Time.now.to_f - t}"
   end
   def setup
-    (init_files2 and @create_file = true) unless @create_file
+    (init_files and @create_file = true) unless @create_file
     @invalid_max_number = 9_000_000_000_0
     @invalid_char_number = '9_x00_000_000_0'
     @map = Bitmapper.new(@index_len)
   end
 
   def test_get_indexes
-    @map.add_from_file in_num_file
+    @map.add in_num_file
     assert_equal @map.get_indexes.length, @assert_index_len
   end
 
-  def test_add_from_file
-    @map.add_from_file in_num_file
+  def test_add
+    @map.add in_num_file
     numbers = `cat #{in_num_file}`.split("\n")
     numbers.each do |i|
       assert_equal @map.status?(i), true
     end
   end
 
-  def test_remove_from_file
-    @map.add_from_file in_num_file
-    @map.remove_from_file in_num_file
+  def test_remove
+    @map.add in_num_file
+    @map.remove in_num_file
     numbers = `cat #{in_num_file}`.split("\n")
     numbers.each do |i|
       assert_equal @map.status?(i), false
     end
   end
 
-  def test_dump_to_file
-    @map.add_from_file in_num_file
-    @map.dump_to_file out_num_file
+  def test_dump_to
+    @map.add in_num_file
+    @map.dump_to out_num_file
     assert_equal `bash -c "diff <(sort #{in_num_file}|uniq) <(sort #{out_num_file}|uniq)"`, ''
   end
 
@@ -104,7 +104,7 @@ class BitmapperTest < Test::Unit::TestCase
   end
 
   def test_status
-    @map.add_from_file in_num_file
+    @map.add in_num_file
     first_10_num = `head -n10 #{in_num_file}`.split("\n")[0..10]
     first_10_num.each do |i|
       assert_equal @map.status?(i), true
@@ -134,12 +134,12 @@ class BitmapperTest < Test::Unit::TestCase
 
   def test_full_run
     with_time_count('add') do
-      @map.add_from_file in_num_file
+      @map.add in_num_file
     end
-    @map.dump_bucket_string out_str_file, @map.get_indexes.first
-    @map.load_string_to_bucket out_str_file, @map.get_indexes.first
+    @map.dump_to_str out_str_file, @map.get_indexes.first
+    @map.load_from_str out_str_file, @map.get_indexes.first
     with_time_count('dump') do
-      @map.dump_to_file out_num_file
+      @map.dump_to out_num_file
     end
     first_10_num = `head -n10 #{in_num_file}`.split("\n")[0..10]
     first_10_num.each do |i|
@@ -148,8 +148,8 @@ class BitmapperTest < Test::Unit::TestCase
     assert_equal `bash -c "diff <(sort #{in_num_file}|uniq) <(sort #{out_num_file}|uniq)"`, ''
     assert_equal @map.get_indexes.length, @assert_index_len
 
-    @map.remove_from_file in_num_file
-    @map.dump_to_file out_num_file
+    @map.remove in_num_file
+    @map.dump_to out_num_file
 
     assert_not_equal `bash -c "diff <(sort #{in_num_file}|uniq) <(sort #{out_num_file}|uniq)"`,''
 
